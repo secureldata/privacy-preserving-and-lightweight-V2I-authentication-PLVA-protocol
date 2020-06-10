5d61e27dbf5c8515db309ec12d41fe6f06a821bf
BT7274 <32004979+secureldata@users.noreply.github.com>
2020-06-10 20:13:05 +0800

    Update PLVA_scheme.java

--- PLVA_scheme.java
+++ PLVA_scheme.java
@@ -1,5 +1,7 @@
 import java.math.BigInteger;
+import java.security.MessageDigest;
 import java.security.SecureRandom;
+import java.util.Random;
 
 import it.unisa.dia.gas.jpbc.Element;
 import it.unisa.dia.gas.jpbc.Field;
@@ -7,7 +9,10 @@ import it.unisa.dia.gas.jpbc.PairingParameters;
 import it.unisa.dia.gas.plaf.jpbc.pairing.a1.TypeA1CurveGenerator;
 import it.unisa.dia.gas.plaf.jpbc.pairing.a1.TypeA1Pairing;
 import it.unisa.dia.gas.plaf.jpbc.util.math.BigIntegerUtils;
-
+import org.bouncycastle.jce.provider.JCEMac;
+import org.bouncycastle.jce.provider.symmetric.AES;
+import javax.swing.*;
+import java.lang.Runtime;
 public class BGNEncryption {
 
 	public static final String start = "start";
@@ -21,13 +26,13 @@ public class BGNEncryption {
 	public PublicKey gen(int bits) {
 		rng = new SecureRandom();
 		TypeA1CurveGenerator a1 = new TypeA1CurveGenerator(rng, 2, bits); // Requires
-																			// 2
-																			// prime
-		//ecc cruve																	// numbers.
+		// 2
+		// prime
+		//typeA1为椭圆曲线，但在不同域上为合数双线性群																	// numbers.
 		param = a1.generate();
 		TypeA1Pairing pairing = new TypeA1Pairing(param);
 		order = param.getBigInteger("n"); // Must extract the prime numbers for
-											// both keys.
+		// both keys.
 		r = param.getBigInteger("n0");
 		q = param.getBigInteger("n1");
 		Field<?> f = pairing.getG1();
@@ -42,7 +47,7 @@ public class BGNEncryption {
 	public Element encrypt(PublicKey PK, int msg) {
 		BigInteger t = BigIntegerUtils.getRandom(PK.getN());
 		int m = msg;
-		System.out.println("Hash is " + m);
+		//System.out.println("Hash is " + m);
 		Field<?> f = PK.getField();
 		Element A = f.newElement();
 		Element B = f.newElement();
@@ -55,23 +60,7 @@ public class BGNEncryption {
 		C = C.add(B);
 		return C;
 	}
-	public Element encryptstr(PublicKey PK, String msg) {
-		BigInteger t = BigIntegerUtils.getRandom(PK.getN());
-		String m = msg;
-		System.out.println("Hash is " + m);
-		Field<?> f = PK.getField();
-		Element A = f.newElement();
-		Element B = f.newElement();
-		Element C = f.newElement();
-		A = A.set(PK.getP());
-		Integer s=Integer.valueOf(m);
-		A = A.mul(BigInteger.valueOf(s));
-		B = B.set(PK.getQ());
-		B = B.mul(t);
-		C = C.set(A);
-		C = C.add(B);
-		return C;
-	}
+
 	public Element add(PublicKey PK, Element A, Element B) {
 		BigInteger t = BigIntegerUtils.getRandom(PK.getN());
 		Field<?> f = PK.getField();
@@ -94,7 +83,6 @@ public class BGNEncryption {
 		K = K.pow(t);
 		return T.mul(K);
 	}
-
 	public String decryptMul(PublicKey PK, BigInteger sk, Element C) {
 		Element PSK = PK.doPairing(PK.getP(), PK.getP());
 		PSK.pow(sk);
@@ -132,62 +120,100 @@ public class BGNEncryption {
 		}
 		return m.toString();
 	}
-	public String StrToBinstr(String str) {
-		char[] strChar = str.toCharArray();
-		String result = "";
-		for (int i = 0; i < strChar.length; i++) {
-			result += Integer.toBinaryString(strChar[i]) + " ";
-		}
-		return result;
-	}
-
-	public String[] StrToStrArray(String str) {
-		return str.split(" ");
-	}
-
 	public static void main(String[] args) {
 		BGNEncryption b = new BGNEncryption();
-		PublicKey PK = b.gen(32);
-		Element Final_result;
+		PublicKey PK = b.gen(256);
 
-		Element key1 = b.encryptstr(PK, "0001");
-		System.out.println("Final_result: " + b.StrToBinstr("0001"));//Key values are in different card slots
-		Element key2 = b.encryptstr(PK, "0020");
-		System.out.println("Final_result: " + b.StrToBinstr("0020"));
-		Element key3 = b.encryptstr(PK, "0300");
-		System.out.println("Final_result: " + b.StrToBinstr("0300"));
-		Element key4 = b.encryptstr(PK, "4000");
-		System.out.println("Final_result: " + b.StrToBinstr("4000"));
-
-		Element en1 = b.encrypt(PK, 1);//1 means vehicle will pass
-		Element en0 = b.encrypt(PK, 0);//0 means the vehicle does not pass
-
-		Element result1 = b.mul(PK, key1, en1);
-		Element result2 = b.mul(PK, key2, en1);
-		Element result3 = b.mul(PK, key3, en1);
-		Element result4 = b.mul(PK, key4, en0);
-
-		Final_result=result1.mul(result2);//
-		Final_result=Final_result.mul(result3);//
-		Final_result=Final_result.mul(result4);//a secure vector product by BGN
-		// Privacy preserving back-propagation neural network learning made practical with cloud computing
-		System.out.println("Final_result: " + b.decryptMul(PK, b.q, Final_result));
-		System.out.println("Final_result: " + b.StrToBinstr(b.decryptMul(PK, b.q, Final_result)));
-
-		String result[]=new String[4];
-		Integer Finalresult[]=new Integer[4];
-		result=	b.StrToStrArray(b.StrToBinstr(b.decryptMul(PK, b.q, Final_result)));
-
-		System.out.println(result.length);
-		if(result.length<4)
+		int n=10;
+		int onevalue=0;
+		int key[]=new int[n];
+		int matrix[][]=new int[n][n];
+		Element Ematrix[][]=new Element[n][n];
+		Element Ekey[]=new Element[n];
+		Element EC[]=new Element[n];
+		Element resultend[]=new Element[n];
+		Element one=b.encrypt(PK,1);
+		for(int i=0;i<n;i++)  //Assuming key vector generation
 		{
-			for(int i=0;i<result.length;i++) {
-				Finalresult[i] = Integer.valueOf(result[i], 2) - 48;
-				System.out.println("Final_getkey: " + Finalresult[i]);
+			key[i]=i+1;
+		}
+		//###############################################################################################
+		//Vehicle pre-calculation
+		//###############################################################################################
+		for(int i=0;i<n;i++)  //The paths traversed are all set to 1, in order to test the calculation time
+		{
+			EC[i]=b.encrypt(PK,1);
+		}
+
+		//###############################################################################################
+		//Calculate cyclic displacement matrix
+		//###############################################################################################
+		for(int i=0;i<n;i++) {  //Calculate the cyclic displacement matrix where the displacement value is set to 2
+			for (int j = 0; j < n; j++) {
+				if(j==(i+(n-2))%n)
+				{
+					matrix[i][j]=1;
+				}
+				else
+				{
+					matrix[i][j]=0;
+				}
 			}
 		}
-		for(int i=0;i<4-result.length;i++){
-			System.out.println("Final_getkey: " + "0");//Finally get the result
+		
+		//###############################################################################################
+		//Encrypted first line
+		//###############################################################################################
+		for(int j=0;j<n;j++)  //To encrypt the circular matrix, first encrypt the first row
+		{
+			Ematrix[0][j] = b.encrypt(PK,matrix[0][j]);
+		}
+		//###############################################################################################
+		//Displacement matrix
+		//###############################################################################################
+		for(int i=0;i<n-1;i++) { //Number of remaining lines, shifted from the first line
+			Element temp=Ematrix[i][n-1];
+
+			for (int j = n-1; j >0; j--) {
+				Ematrix[i+1][j]=Ematrix[i][j-1];
+			}
+			Ematrix[i+1][0]=temp;
+
+		}
+		
+		//###############################################################################################
+		//CA gets the encryption matrix and path vector
+		//###############################################################################################
+		
+		for(int i=0;i<n;i++) {   //CA receives the encryption matrix and combines the encryption matrix into an encryption vector
+			for (int j = 0, s=0; s < n && j<n; j++,s++) {
+				matrix[i][j]=matrix[i][j]*key[s];
+				if(matrix[i][j]!=0)
+				{
+					int number=matrix[i][j];
+					Ematrix[i][j]=b.encrypt(PK,0);
+					while(number>0)
+					{
+						Ematrix[i][j]=Ematrix[i][j].mul(one);
+						number--;
+					}
+					Ekey[i]=Ematrix[i][j];
+				}
+			}
+
+		}
+		for(int i=0;i<n;i++) //Multiply the encrypted vector and the encrypted path vector to the final result vector
+		{
+			resultend[i]=b.mul(PK,EC[i],Ekey[i]);
+		}
+		
+		//###############################################################################################
+		//Vehicle decryption
+		//###############################################################################################
+		for(int i=0;i<n;i++)
+		{
+			
+			b.decryptMul(PK, b.q, resultend[i]);
 		}
 	}
 }
